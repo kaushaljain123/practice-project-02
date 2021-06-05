79d933e3e0cd972bf23fabafa142569dd8d69d52
kaushalhub <jainkaushal123465@gmail.com>
2021-06-05 20:17:10 +0530

    local

--- controllers/product.js
+++ controllers/product.js
@@ -164,8 +164,6 @@ exports.deleteProduct = asyncHandler (async (req, res, next) => {
 //@access       Privaet
 exports.uploadProductPhoto = asyncHandler (async (req, res, next) => {
 
-  
-  
 if(!req.files){
   const error= new Error('please choose files')
   error.httpStatusCode =400;
