2cef1e179a7ce0c23ca8d07b028208136dde1b51
Bringonstore <84062175+myofflinemarket@users.noreply.github.com>
2022-03-14 17:50:32 +0530

    update profile request

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ const { protect, authorize } = require('../middleware/auth');
 
 router.post("/loginviaOtp", loginViaOtp);
 router.post("/verifyOtp", verifyOtp);
-router.post("/me", protect, getMe);
+router.get("/me", protect, getMe);
 router.post("/myshop", protect, getMyShop);
 router.put("/updatedetails", protect, updateDetails);
 router.post("/profile", protect, addProfile);
